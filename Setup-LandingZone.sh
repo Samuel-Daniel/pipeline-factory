cd7fc2d9923760d5c07431a701d358b5bcbb88a6
Henrique Bueno <buenoh@amazon.com>
2019-06-09 13:14:33 -0300

    adicionando script

---
+++ Setup-LandingZone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ProducteName="iti"
+FeatureName="onboarding"
+
+# Product Accounts
+DevToolsAccount="935187548418"
+DevAccount="255538382414"
+HomologAccount="450454659259"
+ProdAccount="754190108602"
+
+#Shared Account
+DevOpsAccount="300003219274"
+ArchitectureAccount="000000000000"
+
